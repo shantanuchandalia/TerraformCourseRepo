f9ff92d6d4c85c1cb5c2bfd5562265b6c50208be
Shantanu Chandalia <shantanu.chandalia@gmail.com>
2020-05-11 12:52:50 +0000

    First Commit

---
+++ t2_micro_basic/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-08bc77a2c7eb2b1da"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-0d729a60"
+  }
+}
+
